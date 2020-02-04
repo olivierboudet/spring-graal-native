56045f9c59b645452698bdb710703fc0393153f7
Olivier Boudet <olivier.boudet@devmachine.fr>
2020-02-04 23:08:55 +0100

    update sample spring-data-mongodb

--- spring-graal-native-samples/spring-data-mongodb/compile.sh
+++ spring-graal-native-samples/spring-data-mongodb/compile.sh
@@ -1,40 +1,56 @@
 #!/usr/bin/env bash
-../../mvnw clean install
 
-export JAR="spring-data-mongodb-0.0.1-SNAPSHOT.jar"
-rm -f clr
-printf "Unpacking $JAR"
-rm -rf unpack
-mkdir unpack
-cd unpack
-jar -xvf ../target/$JAR >/dev/null 2>&1
+ARTIFACT=spring-data-mongodb
+MAINCLASS=com.example.springdatamongodb.SpringDataMongodbApplication
+VERSION=0.0.1-SNAPSHOT
+FEATURE=../../../../spring-graal-native-feature/target/spring-graal-native-feature-0.6.0.BUILD-SNAPSHOT.jar
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+rm -rf target
+mkdir -p target/native-image
+
+echo "Packaging $ARTIFACT with Maven"
+mvn -DskipTests package > target/native-image/output.txt
+
+JAR="$ARTIFACT-$VERSION.jar"
+rm -f $ARTIFACT
+echo "Unpacking $JAR"
+cd target/native-image
+jar -xvf ../$JAR >/dev/null 2>&1
 cp -R META-INF BOOT-INF/classes
 
-cd BOOT-INF/classes
-export LIBPATH=`find ../../BOOT-INF/lib | tr '\n' ':'`
-export CP=.:$LIBPATH
+LIBPATH=`find BOOT-INF/lib | tr '\n' ':'`
+CP=BOOT-INF/classes:$LIBPATH:$FEATURE
 
-# Our feature being on the classpath is what triggers it
-export CP=$CP:../../../../../spring-graal-native-feature/target/spring-graal-native-feature-0.6.0.BUILD-SNAPSHOT.jar
-
-printf "\n\nCompile\n"
-~/softwares/graalvm-ce-java11-19.3.0/bin/native-image \
-  -Dio.netty.noUnsafe=true \
+GRAALVM_VERSION=`native-image --version`
+echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
+{ time native-image \
+  --verbose \
   --no-server \
-  -H:Name=springdatamongodb \
-  -H:+ReportExceptionStackTraces \
   --no-fallback \
+  -DavoidLogback=true \
+  -Ddebug=true \
+  -Dio.netty.noUnsafe=true \
+  -H:Name=$ARTIFACT \
+  -H:+ReportExceptionStackTraces \
   --allow-incomplete-classpath \
+  -H:+TraceClassInitialization \
   --report-unsupported-elements-at-runtime \
+  --initialize-at-build-time=org.springframework.util.unit.DataSize \
   -DremoveUnusedAutoconfig=true \
-  -cp $CP com.example.springdatamongodb.SpringDataMongodbApplication
+  -cp $CP $MAINCLASS >> output.txt ; } 2>> output.txt
 
-mv springdatamongodb ../../..
-
-printf "\n\nJava exploded jar\n"
-time java -classpath $CP com.example.springdatamongodb.SpringDataMongodbApplication
-
-printf "\n\nCompiled app (clr)\n"
-cd ../../..
-time ./springdatamongodb
+if [[ -f $ARTIFACT ]]
+then
+  printf "${GREEN}SUCCESS${NC}\n"
+  mv ./$ARTIFACT ..
+  exit 0
+else
+  cat output.txt
+  printf "${RED}FAILURE${NC}: an error occurred when compiling the native-image.\n"
+  exit 1
+fi
 
